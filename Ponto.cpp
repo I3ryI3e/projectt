e3b1c2f330c435b78d64027b59474236f57810ee
fclfi <kikocarlos96@gmail.com>
2017-11-19 12:11:39 +0000

    Ponto updated

--- Ponto.cpp
+++ Ponto.cpp
@@ -2,7 +2,15 @@
 
 Ponto::Ponto() {
 }
-
+int Ponto::getX() const{return x;}
+int Ponto::getY() const{return y;}
+void Ponto::setX(int novox){x = novox;}
+void Ponto::setY(int novoy){y = novoy;}
+string Ponto::obtemXY() const{
+    ostringstream aux;
+    aux << "Ponto (" << x << "," << y << ")";
+    return aux.str();
+}
 Ponto::Ponto(const Ponto& orig) {
 }
 
